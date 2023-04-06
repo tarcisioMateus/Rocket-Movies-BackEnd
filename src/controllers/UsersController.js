dd91b8422bfca6e91ed7498b9f6870309a14ec92
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-04-06 12:36:04 -0300

    created method update{} on UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -18,6 +18,39 @@ class UsersController {
 
         return response.status(201).json()
     }
+
+    async update (request, response) {
+        const { name, email, currentPassword, newPassword } = request.body
+        const { id } = request.params
+
+        const user = await knex('users').where({ id }).first()
+
+        if ( email ) {
+            const userWithEmail = await knex('users').where({ email }).first()
+
+            if ( userWithEmail && userWithEmail.id != user.id ) throw new appError('This Email belongs to another person')
+
+            user.email = email
+        }
+
+        if ( newPassword ) {
+            if ( !currentPassword ) throw new appError('You must provid your current password to updated it')
+
+            const passwordCheck = await compare(currentPassword, user.password)
+
+            if ( passwordCheck ) {
+                user.password = await hash (newPassword, 8)
+            } else {
+                throw new appError("The given password doesn't match!" )
+            }
+        }
+        
+        user.name = name ? name : user.name
+
+        await knex('users').update({ name: user.name, email: user.email, password: user.password })
+
+        return response.json()
+    }
 }
 
 module.exports = UsersController
