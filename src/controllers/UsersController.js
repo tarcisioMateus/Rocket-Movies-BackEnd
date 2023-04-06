d3f7bfeffa78514f9be2235011ecebc5a4643171
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-04-06 12:08:39 -0300

    created method create{} on UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,13 +2,21 @@ const knex = require('../database/knex')
 
 const { hash, compare } = require('bcryptjs')
 
+const appError = require('../utils/appError')
+
 class UsersController {
     async create (request, response) {
         const { name, email, password } = request.body
 
         const userWithEmail = await knex('users').where({ email }).first()
 
-        if ( userWithEmail ) 
+        if ( userWithEmail ) throw new appError('Email already in use!')
+
+        const cryptedPassword = await hash(password, 8)
+
+        await knex('users').insert({ name, email, password: cryptedPassword})
+
+        return response.status(201).json()
     }
 }
 
