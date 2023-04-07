e222b5c1cd664e51b51f75c4bb1f6dc550af9c11
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-04-07 12:21:37 -0300

    created method index{} on MovieNotesController

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -29,6 +29,53 @@ class MovieNotesController {
 
         return response.json(note)
     }
+
+    async index (request, response) {
+        const { title, tags } = request.body
+        const { user_id } = request.params
+
+        let notes
+
+        if ( tags ) {
+            const Tags = tags.split(',').map( tag => tag.toLowerCase().trim())
+
+            if ( title ) {
+                notes = await knex('movie_tags').select(['movie_notes.id', 'movie_notes.title', 'movie_notes.rating'])
+                .where('movie_notes.user_id', user_id)
+                .whereLike('movie_notes.title', `%${title}%`)
+                .whereIn('movie_tags.name', Tags)
+                .InnerJoin('movie_notes', 'movie_notes.id', 'movie_tags.note_id')
+                .orderBy('movie_notes.title')
+            } else {
+                notes = await knex('movie_tags').select(['movie_notes.id', 'movie_notes.title', 'movie_notes.rating'])
+                .where('movie_notes.user_id', user_id)
+                .whereIn('movie_tags.name', Tags)
+                .InnerJoin('movie_notes', 'movie_notes.id', 'movie_tags.note_id')
+                .orderBy('movie_notes.title')
+            }
+        } else {
+            notes = await knex('movie_notes').where({ user_id }).whereLike('title', `%${title}%`)
+        }
+
+        let notesOneEntryOnly = []
+        for (let note of notes) {
+            if ( notesOneEntryOnly.filter( nt => nt.id == note.id).length ) continue
+            notesOneEntryOnly = [ ...notesOneEntryOnly, note]
+        }
+
+        let userTags = await knex('movie_tags').where({ user_id })
+
+        const notesWithTags = notesOneEntryOnly.map( note => {
+            const note_tags = userTags.filter( tag => tag.note_id == note_id).map( tag => tag.name)
+
+            return {
+                ...note,
+                tags: note_tags
+            }
+        })
+
+        return response.json(notesWithTags)
+    }
 }
 
 module.exports = MovieNotesController
