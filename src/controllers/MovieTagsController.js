6a174644b0ec871bf2b25f5059aef585c88159f0
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-04-08 10:28:50 -0300

    created method index{} on MovieTagsController

---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,18 @@
+const knex = require('../database/knex')
+
+class MovieTagsController {
+    async index (request, response) {
+        const { user_id } = request.params
+
+        const tags = await knex('movie_tags').where({ user_id }).orderBy('name').map(tag => tag.name)
+
+        let filterdTags = []
+        for (let tag of tags) {
+            if (filterdTags.includes(tag)) continue
+            filterdTags = [ ...filterdTags, tag]
+        }
+        return response.json(filterdTags)
+    }
+}
+
+module.exports = MovieTagsController
