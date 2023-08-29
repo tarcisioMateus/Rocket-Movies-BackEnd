a3e24731357fe846609482503036a1cb5e18cd38
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-29 10:28:55 -0300

    implemented middleware Authentication movie_tags.routes

--- src/routes/movie_tags.routes.js
+++ src/routes/movie_tags.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const MovieTagsController = require('../controllers/MovieTagsController')
 const movieTagsController = new MovieTagsController()
 
 const movieTagsRoutes = Router()
 
-movieTagsRoutes.get('/:user_id', movieTagsController.index)
+movieTagsRoutes.get('/', ensureAuthenticated, movieTagsController.index)
 
 module.exports = movieTagsRoutes
