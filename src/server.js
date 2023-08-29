51bf4d52596ca5adcd37d2350707511770e752ad
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-29 18:22:44 -0300

    bug fixxed related to error.response.data.

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ app.use(routes)
 
 app.use( (error, request, response, next) => {
     if ( error instanceof appError ) {
-        return response.status(error.status).json( error.message)
+        return response.status(error.status).json({ message: error.message})
     }
-    return response.status(500).json(`Server Error ${error.message}`)
+    return response.status(500).json({ message: `Server Error ${error.message}`})
 })
 
 const PORT = 3333
