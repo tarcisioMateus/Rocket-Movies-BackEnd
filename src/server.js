23066eadae8266a44c17ed6de1e1ce653f3434b1
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-29 11:16:15 -0300

    implemented new route that displays files uploaded by user

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ require('express-async-errors')
 const express = require('express')
 
 const appError = require('./utils/appError')
-
 const routes = require('./routes')
+const uploadConfig = require('./configs/upload')
 
 const app = express()
 
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 
 app.use( (error, request, response, next) => {
