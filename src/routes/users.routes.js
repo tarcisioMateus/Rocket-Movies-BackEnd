c10c982bceb7e5ea117bfd067727e38dba15ef3e
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-29 11:10:54 -0300

    implemented users.routes.js new route to update avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require('express')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
-const UsersController = require('../controllers/UsersController')
-const usersController = new UsersController()
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
+const UsersController = require('../controllers/UsersController')
+const UsersAvatarController = require('../controllers/UsersAvatarController')
+const usersController = new UsersController()
+const usersAvatarController = new UsersAvatarController()
+
+const upload = multer(uploadConfig.MULTER)
 const usersRoutes = Router()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
 usersRoutes.delete('/', ensureAuthenticated, usersController.delete)
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), usersAvatarController.update)
 
 module.exports = usersRoutes
