f500760b2ddee8f6f801d4ead9c7261d5efaae52
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-09-08 12:13:46 -0300

    created UsersRepository.js

---
+++ src/repositories/UsersRepository.js
@@ -0,0 +1,33 @@
+const knex = require('../database/knex')
+
+class UsersRepository {
+    async findByEmail({ email }) {
+        const user = await knex('users').where({ email }).first()
+        return user
+    }
+
+    async create({ name, email, password }) {
+        const [ id ] = await knex('users').insert({ name, email, password })
+        return { id }
+    }
+
+    async findById({ id }) {
+        const user = await knex('users').where({ id }).first()
+        return user
+    }
+
+    async update({ id, user }) {
+        const updated = await knex('users').where({ id }).update({
+                name: user.name, email: user.email, 
+                avatar: user.avatar, 
+                password: user.password, updated_at: knex.fn.now() 
+            }, ['name', 'email'])
+        return updated
+    }
+
+    async delete({ id }) {
+        await knex('users').where({ id }).delete()
+    }
+}
+
+module.exports = UsersRepository
