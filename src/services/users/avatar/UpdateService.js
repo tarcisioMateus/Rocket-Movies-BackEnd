bd9a039ab6fb511af30abc14a25fbd2c5faf68cc
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-09-09 11:37:35 -0300

    users/avatar/UpdateService.js

---
+++ src/services/users/avatar/UpdateService.js
@@ -0,0 +1,27 @@
+const appError = require('../../../utils/appError')
+
+class UpdateService {
+    constructor({ 
+        usersRepository, diskStorage 
+    }) {
+        this.usersRepository = usersRepository
+        this.diskStorage = diskStorage
+    }
+
+    async execute({ 
+        id, filename
+    })
+    {
+        const user = await this.usersRepository.findById({ id })
+        if (!user) throw new appError('only authenticated users can change avatar ')
+
+        if (user.avatar) await this.diskStorage.deleteFile(user.avatar)
+
+        user.avatar = await this.diskStorage.saveFile(filename) 
+        await this.usersRepository.update({ id, user })
+
+        return filename
+    }
+}
+
+module.exports = UpdateService
