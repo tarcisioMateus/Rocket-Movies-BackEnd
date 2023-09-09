3a2e227a864c6026f2d5eed5bb6e55129f59e7c4
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-09-09 12:55:30 -0300

    updated UsersAvatarController.js

--- src/controllers/UsersAvatarController.js
+++ src/controllers/UsersAvatarController.js
@@ -1,22 +1,25 @@
-const knex = require('../database/knex')
+const UsersRepository = require('../repositories/UsersRepository')
 const DiskStorage = require('../providers/DiskStorage')
-const appError = require('../utils/appError')
+
+const UpdateService = require('../services/users/avatar/UpdateService')
 
 class UsersAvatarController {
+  usersRepository = new UsersRepository()
+  diskStorage = new DiskStorage()
+
   async update(request, response) {
     const user_id = request.user.id
-    const filename = request.file.filename
+    const { filename } = request.file
 
-    const user = await knex('users').where({ id: user_id}).first()
-    if (!user) throw new appError('only authenticated users can change avatar ')
+    const updateService = new UpdateService({
+      usersRepository: this.usersRepository, 
+      diskStorage: this.diskStorage
+    })
+    const updated = await updateService.execute({ 
+      id: user_id, filename
+    })
 
-    const diskStorage = new DiskStorage()
-    if (user.avatar) await diskStorage.deleteFile(user.avatar)
-
-    user.avatar = await diskStorage.saveFile(filename) 
-    await knex('users').update(user).where( {id: user_id})
-
-    return response.json(filename)
+    return response.json( updated )
   }
 }
 
